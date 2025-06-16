d43ea1af4920a7d8ab3f85d5e9a61bd5fdb78bc7
Veerin Bajracharya <verrinshakya309@gmail.com>
2025-06-16 09:41:34 +0545

    PWA minor updated

--- vite.config.js
+++ vite.config.js
@@ -20,12 +20,12 @@ export default defineConfig({
         scope: '/',
         icons:[
           {
-            src: '/icons/android-chrome-192x192.png',
+            src: '/icons/web-app-manifest-192x192.png',
             sizes: '192x192',
             type: 'image/png'
           },
           {
-            src: '/icons/android-chrome-512x512.png',
+            src: '/icons/web-app-manifest-512x512.png',
             sizes: '512x512',
             type: 'image/png'
           }
